3862a6f14a39c95061ac010689e0b713413eee70
Vinicius <labsfiap@fiap.com.br>
2024-10-08 09:58:29 -0300

    alterando provider

--- lib/providers/task_group_provider.dart
+++ lib/providers/task_group_provider.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/models/task_group.dart';
+import 'package:todo_app/repository/supabase_repository.dart';
 
-class TaskGroupProvider {
+class TaskGroupProvider extends ChangeNotifier {
+
+  final supabaseRepo = SupabaseRepository();
 
   bool _isLoading = false;
   bool get isLoading => _isLoading;
+
+  List<TaskGroup> _taskGroups = [];
+  List<TaskGroup> get taskGroup => _taskGroups;
+
+  Future<void> listTasksGroups() async {
+
+    try {
+
+      _isLoading = true;
+      notifyListeners();
+      _taskGroups = await supabaseRepo.listTasksGroups();
+      notifyListeners();
+
+    } catch (e) {
+
+    } finally {
+
+    }
+
+  }
 
 }
